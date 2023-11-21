d0f589a4217bf0fbb3b84b73df9bfcbfafea0d95
EduardoSQ <eduardoooo89@hotmail.com>
2023-11-20 18:24:18 -0600

    Añadir seguimiento al archivo config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+from configparser import ConfigParser
+
+def config(archivo="./yourpc.ini", seccion="mysql"):
+    parser = ConfigParser()
+    parser.read(archivo)
+
+    db = {}
+    if parser.has_section(seccion):
+        params = parser.items(seccion)
+        for param in params:
+            db[param[0]] = param[1]
+        return db
+    else:
+        raise Exception("Sección {0} no encontada en el archivo '{1}'".format(seccion, archivo))
